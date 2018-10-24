31d054393bc82cd209cbe0416d777e3eee5b98d4
Victor Mann <victorcmann@gmail.com>
2018-10-24 16:51:09 -0200

    criando NegociacaoController e importando em index

---
+++ ts/controllers/NegociacaoController.ts
@@ -0,0 +1,28 @@
+class NegociacaoController
+{
+    private _inputData;
+    private _inputQuantidade;
+    private _inputValor;
+
+    constructor ()
+    {
+        this._inputData = document.querySelector('#data');
+        this._inputQuantidade = document.querySelector('#quantidade');
+        this._inputValor = document.querySelector('#valor');
+    }
+
+    adiciona (event)
+    {
+        // evitando a submissão do formulário
+        event.preventDefault();
+
+        // instanciando uma negociação
+        const negocicao = new Negociacao(
+            this._inputData.value,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negocicao);
+    }
+}
