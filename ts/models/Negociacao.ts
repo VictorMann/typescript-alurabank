9b90a51a5e245db028a1f15050c5349de751cc50
Victor Mann <victorcmann@gmail.com>
2018-10-24 17:10:02 -0200

    alterando propriedade aplicando atalho

--- ts/models/Negociacao.ts
+++ ts/models/Negociacao.ts
@@ -2,16 +2,8 @@ class Negociacao
 {
     // declaração das propriedades de classe
     // typescript
-    private _data;
-    private _quantidade;
-    private _valor;
 
-    constructor (data, quantidade, valor)
-    {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    constructor (private _data: Date, private _quantidade: number, private _valor: number) {}
 
     get data ()
     {
