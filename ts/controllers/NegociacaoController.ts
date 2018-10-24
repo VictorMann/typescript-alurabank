3fef24b778c152cbec6916161e99b9a4151f3335
Victor Mann <victorcmann@gmail.com>
2018-10-24 19:13:42 -0200

    definindo tipos especificos em NegociacaoController com casting

--- ts/controllers/NegociacaoController.ts
+++ ts/controllers/NegociacaoController.ts
@@ -1,26 +1,28 @@
 class NegociacaoController
 {
-    private _inputData;
-    private _inputQuantidade;
-    private _inputValor;
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
 
     constructor ()
     {
-        this._inputData = document.querySelector('#data');
-        this._inputQuantidade = document.querySelector('#quantidade');
-        this._inputValor = document.querySelector('#valor');
+        // casting : <HTMLInputElement>
+        // convertendo para um tipo mais específico
+        this._inputData = <HTMLInputElement>document.querySelector('#data');
+        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
     }
 
-    adiciona (event)
+    adiciona (event: Event)
     {
         // evitando a submissão do formulário
         event.preventDefault();
 
         // instanciando uma negociação
         const negocicao = new Negociacao(
-            this._inputData.value,
-            this._inputQuantidade.value,
-            this._inputValor.value
+            new Date(this._inputData.value.replace(/-/g, ',')),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
         );
 
         console.log(negocicao);
