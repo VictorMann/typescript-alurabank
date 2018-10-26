57fab34994c70b04c3fdc89cfe3cf75b8ebd7100
Victor Mann <victorcmann@gmail.com>
2018-10-26 18:41:03 -0200

    alterando a manipulacao do dom com jquery nas classes

--- ts/controllers/NegociacaoController.ts
+++ ts/controllers/NegociacaoController.ts
@@ -1,8 +1,8 @@
 class NegociacaoController
 {
-    private _inputData: HTMLInputElement;
-    private _inputQuantidade: HTMLInputElement;
-    private _inputValor: HTMLInputElement;
+    private _inputData: JQuery;
+    private _inputQuantidade: JQuery;
+    private _inputValor: JQuery;
     // o ts define por si só o tipo ref a instancia _negocicoes :Negociacoes
     private _negociacoes = new Negociacoes;
     private _negociacoesView = new NegociacoesView('#negociacoesView');
@@ -12,9 +12,9 @@ class NegociacaoController
     {
         // casting : <HTMLInputElement>
         // convertendo para um tipo mais específico
-        this._inputData = <HTMLInputElement>document.querySelector('#data');
-        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
-        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
         // renderiza
         this._negociacoesView.update(this._negociacoes);
     }
@@ -26,9 +26,9 @@ class NegociacaoController
 
         // instanciando uma negociação
         const negocicao = new Negociacao(
-            new Date(this._inputData.value.replace(/-/g, ',')),
-            parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value)
+            new Date(this._inputData.val().replace(/-/g, ',')),
+            parseInt(this._inputQuantidade.val()),
+            parseFloat(this._inputValor.val())
         );
         
         // add negocicao à lista
--- ts/views/View.ts
+++ ts/views/View.ts
@@ -1,20 +1,23 @@
+// tornando jQuery public
+// declare var $: any;
+
 // <T> sigfica um tipo que as classes filhas podem definir
 // para passar aos metodos herdados não sendo um tipo unico em cada classe que
 // herdar aquele metodo 
 abstract class View<T>
 {
-    private _elemento: Element;
+    private _elemento: JQuery;
 
     constructor (seletor: string)
     {
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
 
     // T define um tipo como string, number, ou ate classes...
     // que as herdeiras definem em sua assinatura
     update (model: T): void
     {
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
 
     // alternativa para dizer metodo obrigatório a ser definido
